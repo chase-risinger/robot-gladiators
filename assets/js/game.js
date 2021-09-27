11ec0c86f295e1d1a398bd039e6d9afd491a22eb
chase-risinger <ccr1988@gmail.com>
2021-09-27 16:39:30 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,9 +71,17 @@ var fight = function (enemyName) {
 
 
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to robot gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost, game over sorry");
+        break;
+    }
+
 }
 
 // fight();
